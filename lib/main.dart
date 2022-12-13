89fa1139bcf908782b07b8697e3e3f8a76ebe6e9
RazanHammad97 <razanhammadq@gmail.com>
2022-12-13 11:40:39 +0200

    First Commit add music files

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Expanded(
+            child: Container(
+             color: Colors.red,
+
+
+            ),
+
+          ),
+
+
+        ),
+      ),
+    );
+  }
+}
